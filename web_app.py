b18a344da668ca3ecef9632d7719aac69c50bd55
kodemasta <bsheehan.biz@gmail.com>
2026-04-17 22:08:17 +0000

    Remove HTML verification file: use meta tag verification instead for Streamlit

--- web_app.py
+++ web_app.py
@@ -444,6 +444,7 @@ def main():
         <meta property="og:type" content="website">
         <meta name="author" content="Bob Sheehan">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="google-site-verification" content="google3da81ff67ceba959" />
         <script type="application/ld+json">
         {
             "@context": "https://schema.org",
